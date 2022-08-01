8b3bf4e5281cd41efad8869d748441f2c3e80f06
bfiadonu <bfiadonu@gmail.com>
2022-08-01 18:19:34 -0400

    third

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -11,7 +11,7 @@ export const searchMovie = text => dispatch => {
 }
 
 export const fetchMovies = text => dispatch => {
-    axios.get(`http://www.omdbapi.com/?apikey=8288acfd&s=${text}`)
+    axios.get(`https://www.omdbapi.com/?apikey=8288acfd&s=${text}`)
     .then(response => dispatch ({
         type: FETCH_MOVIES,
         payload: response.data.Search
@@ -21,7 +21,7 @@ export const fetchMovies = text => dispatch => {
     }
 
     export const fetchMovie = id => dispatch => {
-        axios.get(`http://www.omdbapi.com/?apikey=8288acfd&i=${id}`)
+        axios.get(`https://www.omdbapi.com/?apikey=8288acfd&i=${id}`)
         .then(response => dispatch ({
             type: FETCH_MOVIE,
             payload: response.data
